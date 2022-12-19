097338ff7b1726bcc92c2b33d8594b86f51097cd
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-12-19 18:33:15 +0200

    import data from csv file and make some operations

---
+++ stats/src/MatchReader.ts
@@ -0,0 +1,23 @@
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchData } from "./MatchData";
+import { MatchResult } from "./MatchResult";
+import { dateStringToDate } from "./utils";
+
+
+export class MatchReader extends CsvFileReader<MatchData> {
+
+  matches: MatchData[] = [];
+
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult, // type assertion
+      row[6]
+    ];
+  }
+
+}
